b6d433f513b87509b173b67039a6eb28964c8902
kunakatadiwanashe99@gmail.com <kunakatadiwanashe99@gmail.com>
2024-10-28 02:04:46 +0200

    auth working

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    name: {type: String, trim: true},
-    email: {type: String, required: true, unique: true, trim: true}
-}, {timestamps: true});
-
-const User = mongoose.models.User || mongoose.model("User", userSchema)
-export default User
