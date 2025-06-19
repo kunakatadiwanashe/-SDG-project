98618c46a55f6c91bf71538f26224c1287942397
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2025-06-19 15:48:23 +0200

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html  lang="en" className={`${roboto.variable} font-sans`}>
+      <head><link rel="icon" href="/favicon.png" type="image/png" /></head>
       <body suppressHydrationWarning={true}>
         <SessionProvider>
           {children}
